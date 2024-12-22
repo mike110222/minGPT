c26f9cf164cbb8922ee1327c06c12383df8e3160
mike_1102 <michael_1102@hotmail.com>
2024-12-23 03:02:26 +0800

    a

--- mingpt/model.py
+++ mingpt/model.py
@@ -24,7 +24,7 @@ class NewGELU(nn.Module):
     Reference: Gaussian Error Linear Units (GELU) paper: https://arxiv.org/abs/1606.08415
     """
     def forward(self, x):
-        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))
+        return 0.5 * x * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (x + 0.044715 * torch.pow(x, 3.0))))  # approximation formula of GELU
 
 class CausalSelfAttention(nn.Module):
     """
